ec3dd85aa05483c4ee4ec4e1692aabf5146b89fa
akeshavan <keshavan@berkeley.edu>
2018-09-14 14:16:33 -0400

    fix: ross put everything on github. cool

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   needsTutorial: false,
   allSrcs: 'https://raw.githubusercontent.com/boutiques/afni-integration/master/docs/to_boutify/all_programs',
-  imageBaseUrl: 'https://s3.amazonaws.com/afnihelp/',
+  imageBaseUrl: 'https://raw.githubusercontent.com/boutiques/afni-integration/master/data/boutify/',
   imageExt: 'json',
   selectionMin: 5,
   N: 1730,
