65313d8d105ada5f9f33112589bb415a3a0e193a
akeshavan <keshavan@berkeley.edu>
2018-09-14 11:43:45 -0400

    wip/enh: adding a code editor to be a boutiques descriptor text entry

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,19 +2,48 @@
   <div id="sidebar" :style="getSidebarStyle" class="container">
     <h3 class="mt-1 mb-1"> {{param}} </h3>
     <small style="text-muted pb-2 mb-2"> Enter the boutiques descriptor:</small>
-    <b-form-textarea id="textarea1"
+    <!-- <b-form-textarea id="textarea1"
                  v-model="json"
                  placeholder="Enter boutiques descriptor for this parameter"
                  :rows="20"
                  :max-rows="60">
-    </b-form-textarea>
+    </b-form-textarea> -->
+    <!-- <pre id="editor">function foo(items) {
+        var i;
+        for (i = 0; i &lt; items.length; i++) {
+            alert("Ace Rocks " + items[i]);
+        }
+    }</pre> -->
+
+    <!-- <editor height="200px" :content="json"> </editor> -->
+
+    <AceEditor
+      :fontSize="14"
+      :showPrintMargin="true"
+      :showGutter="true"
+      :highlightActiveLine="true"
+      mode="javascript"
+      theme="monokai"
+      :onChange="onChange"
+      name="editor"
+
+    />
+
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import brace from 'brace';
+import { Ace as AceEditor, Split as SplitEditor } from 'vue2-brace-editor';
+
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
+
 export default {
   name: 'sidebar',
   props: ['param', 'sidebarStyle'],
+  components: { AceEditor },
   data() {
     return {
       json: '',
@@ -22,6 +51,9 @@ export default {
   },
   mounted() {
     console.log('the color we get is', this.sidebarStyle);
+    // const editor = ace.edit("editor");
+    // editor.setTheme("ace/theme/twilight");
+    // editor.session.setMode("ace/mode/javascript");
   },
   computed: {
     getSidebarStyle() {
@@ -54,5 +86,9 @@ export default {
       position: fixed;
       width: 30%;
       z-index: 2;
+  }
+
+  #editor {
+    width: unset !important;
   }
 </style>
